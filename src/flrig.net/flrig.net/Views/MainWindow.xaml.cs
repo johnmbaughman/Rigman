399aaaf15ae834ded5b67575b10c76749321af1e
John M. Baughman <1634414+johnmbaughman@users.noreply.github.com>
2020-06-25 17:35:45 -0600

    DI discovery (#12)
    * Discovery of views, ReactiveUI, and AvaloniaUI
    
    * A lot of sample ReactiveUI code

--- src/flrig.net/flrig.net/Views/MainWindow.xaml.cs
+++ src/flrig.net/flrig.net/Views/MainWindow.xaml.cs
@@ -1,25 +1,49 @@
-﻿using System.Diagnostics;
+﻿#if DEBUG
+using System.Diagnostics;
+#endif
+using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Threading;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
-using Splat;
-using flrig.net.lib;
+using Avalonia.ReactiveUI;
+using flrig.net.ViewModels;
+using ReactiveUI;
 
 namespace flrig.net.Views
 {
-    public class MainWindow : Window
+    public class MainWindow : ReactiveWindow<MainWindowViewModel>
     {
         public MainWindow()
         {
+            ViewModel = new MainWindowViewModel();
+
+            this.WhenActivated(disposables =>
+            {
+#if DEBUG
+                Debug.WriteLine($"[v  {Thread.CurrentThread.ManagedThreadId}]: Main View activated\n");
+
+
+                Disposable
+                    .Create(() => Debug.WriteLine($"[v  {Thread.CurrentThread.ManagedThreadId}]: Main View deactivated"))
+                    .DisposeWith(disposables);
+
+                Observable
+                    .FromEventPattern(wndMain, nameof(wndMain.Closing))
+                    .Subscribe(_ => { Debug.WriteLine($"[v  {Thread.CurrentThread.ManagedThreadId}]: Main window closing..."); })
+                    .DisposeWith(disposables);
+#endif
+
+                this
+                    .BindCommand(ViewModel, vm => vm.OnClickCommand, v => v.BtnSerialSettings)
+                    .DisposeWith(disposables);
+            });
+
             InitializeComponent();
 #if DEBUG
             this.AttachDevTools();
-
-            var rig = Locator.Current.GetService<IRigs>();
-            if (rig != null)
-            {
-                Debug.WriteLine(rig.Name);
-            }
 #endif
         }
 
@@ -27,5 +51,9 @@ namespace flrig.net.Views
         {
             AvaloniaXamlLoader.Load(this);
         }
+
+        private Button BtnSerialSettings => this.FindControl<Button>("btnSerialSettings");
+
+        private Window wndMain => this.FindControl<Window>("wndMain");
     }
 }
---
+++ src/flrig.net/flrig.net/Views/SerialSettings.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+#if DEBUG
+using System.Diagnostics;
+#endif
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Threading;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using flrig.net.ViewModels;
+using ReactiveUI;
+
+namespace flrig.net.Views
+{
+    public class SerialSettings : ReactiveWindow<ISerialSettingsViewModel>
+    {
+        public SerialSettings()
+        {
+            this.WhenActivated(disposables =>
+            {
+#if DEBUG
+                Debug.WriteLine($"[v  {Thread.CurrentThread.ManagedThreadId}]: Serial View activated\n");
+
+
+                Disposable
+                    .Create(() => Debug.WriteLine($"[v  {Thread.CurrentThread.ManagedThreadId}]: Serial View deactivated"))
+                    .DisposeWith(disposables);
+
+                Observable
+                    .FromEventPattern(wndSerial, nameof(wndSerial.Closing))
+                    .Subscribe(_ => Debug.WriteLine($"[v  {Thread.CurrentThread.ManagedThreadId}]: Serial window closing..."))
+                    .DisposeWith(disposables);
+#endif
+                this
+                    .OneWayBind(ViewModel, vm => vm.Greeting, v => v.Greeting.Text);
+
+                this
+                    .BindCommand(ViewModel, vm => vm.JustAClick, v => v.BtnClick)
+                    .DisposeWith(disposables);
+            });
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            this.WhenActivated(disposables => { });
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        private TextBlock Greeting => this.FindControl<TextBlock>("Greeting");
+        private Button BtnClick => this.FindControl<Button>("BtnClick");
+        private Window wndSerial => this.FindControl<Window>("wndSerial");
+    }
+}
